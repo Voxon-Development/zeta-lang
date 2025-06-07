9facaa559f40139e8d059eaa4449c8c21cfe9526
FlameyosFlow <flamesfflowsnow@gmail.com>
2025-06-07 23:58:57 +0300

    Add proper optimization pass support

--- zetac/src/codegen/ir/optimization/pass.rs
+++ zetac/src/codegen/ir/optimization/pass.rs
@@ -1,35 +1,91 @@
-use crate::ast;
-
 pub trait Pass {
-    fn optimize(&mut self, stmts: &mut Vec<ast::Stmt>) -> anyhow::Result<()>;
+    fn optimize(&mut self, stmts: &mut Vec<ir::Bytecode>) -> anyhow::Result<()>;
 
-    fn priority(&self) -> u16;
+    fn priority(&self) -> OptimizationPassPriority;
 }
 
-impl From<u16> for PassPriority {
+impl From<u16> for OptimizationPassPriority {
     fn from(value: u16) -> Self {
         match value {
-            5 => PassPriority::Highest,
-            4 => PassPriority::High,
-            3 => PassPriority::Normal,
-            2 => PassPriority::Low,
-            1 => PassPriority::Lowest,
-            _ => PassPriority::Normal
+            5 => OptimizationPassPriority::Max,
+            4 => OptimizationPassPriority::High,
+            3 => OptimizationPassPriority::Medium,
+            2 => OptimizationPassPriority::Low,
+            1 => OptimizationPassPriority::Min,
+            _ => OptimizationPassPriority::Medium
         }
     }
 }
 
-impl From<PassPriority> for u16 {
-    fn from(value: PassPriority) -> Self {
+impl From<OptimizationPassPriority> for u16 {
+    fn from(value: OptimizationPassPriority) -> Self {
         value as u16
     }
 }
 
 #[derive(Copy, Clone, Hash, Eq, PartialEq)]
-pub enum PassPriority {
-    Highest = 5,
+pub enum OptimizationPassPriority {
+    Max = 5,
     High = 4,
-    Normal = 3,
+    Medium = 3,
     Low = 2,
-    Lowest = 1
+    Min = 1
+}
+
+impl OptimizationPassPriority {
+    pub fn to_string(&self) -> String {
+        match self {
+            OptimizationPassPriority::Max => "Max".to_string(),
+            OptimizationPassPriority::High => "High".to_string(),
+            OptimizationPassPriority::Medium => "Normal".to_string(),
+            OptimizationPassPriority::Low => "Low".to_string(),
+            OptimizationPassPriority::Min => "Min".to_string(),
+        }
+    }
+}
+
+impl Default for OptimizationPassPriority {
+    fn default() -> Self {
+        OptimizationPassPriority::Medium
+    }
+}
+
+pub struct ConstantFoldingPass;
+
+impl Pass for ConstantFoldingPass {
+    fn optimize(&mut self, bytecode: &mut Vec<ir::Bytecode>) -> anyhow::Result<()> {
+         for stmt in bytecode.iter_mut() {
+             // TODO: check the bytecode, and if it's a math operation with two constants then let's replace it with a constant
+             
+         }
+        todo!()
+    }
+
+    fn priority(&self) -> OptimizationPassPriority {
+        OptimizationPassPriority::Min
+    }
+}
+
+pub struct DeadCodeEliminationPass;
+
+impl Pass for DeadCodeEliminationPass {
+    fn optimize(&mut self, bytecode: &mut Vec<ir::Bytecode>) -> anyhow::Result<()> {
+        todo!()
+    }
+
+    fn priority(&self) -> OptimizationPassPriority {
+        OptimizationPassPriority::Min
+    }
+}
+
+pub struct InliningPass;
+
+impl Pass for InliningPass {
+    fn optimize(&mut self, bytecode: &mut Vec<ir::Bytecode>) -> anyhow::Result<()> {
+        todo!()
+    }
+
+    fn priority(&self) -> OptimizationPassPriority {
+        OptimizationPassPriority::Max
+    }
 }
--- zetac/src/codegen/ir/optimization/pass_manager.rs
+++ zetac/src/codegen/ir/optimization/pass_manager.rs
@@ -1,9 +1,9 @@
 use std::collections::HashMap;
 use crate::ast;
-use crate::codegen::ir::optimization::pass::{Pass, PassPriority};
+use crate::codegen::ir::optimization::pass::{Pass, OptimizationPassPriority};
 
 pub struct PassManager {
-    passes: HashMap<PassPriority, Vec<Box<dyn Pass>>>
+    passes: HashMap<OptimizationPassPriority, Vec<Box<dyn Pass>>>
 }
 
 impl PassManager {
@@ -13,30 +13,28 @@ impl PassManager {
         }
     }
     
-    pub fn add_pass(&mut self, priority: PassPriority, pass: Box<dyn Pass>) {
+    pub fn add_pass(&mut self, priority: OptimizationPassPriority, pass: Box<dyn Pass>) {
         self.passes.entry(priority).or_insert(Vec::new()).push(pass);
     }
     
-    pub fn run_passes(&mut self, ir: &mut Vec<ast::Stmt>) -> anyhow::Result<()> {
+    pub fn run_passes(&mut self, ir: &mut Vec<ir::Bytecode>) -> anyhow::Result<()> {
+        self.run_passes_with_priority(OptimizationPassPriority::Max, ir)?;
+        self.run_passes_with_priority(OptimizationPassPriority::High, ir)?;
+        self.run_passes_with_priority(OptimizationPassPriority::Medium, ir)?;
+        self.run_passes_with_priority(OptimizationPassPriority::Low, ir)?;
+        self.run_passes_with_priority(OptimizationPassPriority::Min, ir)?;
+        
+        Ok(())
+    }
+    
+    pub fn run_passes_with_priority(&mut self, priority: OptimizationPassPriority, ir: &mut Vec<ir::Bytecode>) -> anyhow::Result<()> {
         // run based on priority
-        for passes in self.passes.get_mut(&PassPriority::Highest).unwrap() {
-            passes.optimize(ir)?;
+        if !self.passes.contains_key(&priority) {
+            return Ok(());
         }
         
-        for passes in self.passes.get_mut(&PassPriority::High).unwrap() {
-            passes.optimize(ir)?;
-        }
-        
-        for passes in self.passes.get_mut(&PassPriority::Normal).unwrap() {
-            passes.optimize(ir)?;
-        }
-        
-        for passes in self.passes.get_mut(&PassPriority::Low).unwrap() {
-            passes.optimize(ir)?;
-        }
-        
-        for passes in self.passes.get_mut(&PassPriority::Lowest).unwrap() {
-            passes.optimize(ir)?;
+        for optimization_pass in self.passes.get_mut(&priority).unwrap() {
+            optimization_pass.optimize(ir)?;
         }
         
         Ok(())
