13a62afb037dcd8ad5e8ae1b970bb223d70a7c07
FlameyosFlow <flamesfflowsnow@gmail.com>
2025-06-14 18:14:56 +0300

    Small changes so that the pass manager gets the data they need.

--- zeta/src/vm/functions.rs
+++ zeta/src/vm/functions.rs
@@ -1,16 +1,18 @@
-use ir::Bytecode;
+use crate::vm::profiler::FunctionCall;
 pub(crate) use zetac::codegen::ir::module::Function;
+use zetac::codegen::ir::module::ZetaModule;
 use zetac::codegen::ir::optimization::pass::OptimizationPassPriority;
 use zetac::codegen::ir::optimization::pass_manager;
-use crate::vm::frames;
-use crate::vm::frames::StackFrame;
-use crate::vm::profiler::FunctionCall;
+
+type NativeFn = fn();
+
+#[allow(dead_code)]
 
 pub(crate) fn run_native_function(function: &Function) {
     let pointer = function.native_pointer.clone().unwrap();
 
     unsafe {
-        let pointer: fn() = std::mem::transmute(pointer);
+        let pointer: NativeFn = std::mem::transmute(pointer);
         pointer();
     }
 }
@@ -19,12 +21,12 @@ pub(crate) fn compile_function(function: &mut Function) {
     function.is_native = true;
 }
 
-pub(crate) fn optimize_function_with_priority(priority: OptimizationPassPriority, function: &mut Function, pass_manager: &mut pass_manager::PassManager) {
-    pass_manager.run_passes_with_priority(priority, &mut function.code).unwrap()
+pub(crate) fn optimize_function_with_priority(priority: OptimizationPassPriority, function: &mut Function, pass_manager: &mut pass_manager::PassManager, module: &ZetaModule) {
+    pass_manager.run_passes_with_priority(priority, &mut function.code, module).unwrap()
 }
 
 #[inline]
-pub(crate) fn optimize_function(function: &mut Function, function_call: &FunctionCall, pass_manager: &mut pass_manager::PassManager) {
+pub(crate) fn optimize_function(function: &mut Function, function_call: &FunctionCall, pass_manager: &mut pass_manager::PassManager, module: &ZetaModule) {
     if function.optimization_level == OptimizationPassPriority::Max {
         return;
     }
@@ -32,7 +34,7 @@ pub(crate) fn optimize_function(function: &mut Function, function_call: &Functio
     let function_call_count = function_call.count;
 
     if function_call_count > 200 {
-        optimize_function_with_priority(OptimizationPassPriority::Max, function, pass_manager);
+        optimize_function_with_priority(OptimizationPassPriority::Max, function, pass_manager, module);
         compile_function(function);
 
         function.optimization_level = OptimizationPassPriority::Max;
@@ -40,7 +42,8 @@ pub(crate) fn optimize_function(function: &mut Function, function_call: &Functio
     if function_call_count > 150 {
         pass_manager.run_passes_with_priority(
             OptimizationPassPriority::High,
-            &mut function.code
+            &mut function.code,
+            module
         ).unwrap();
 
         compile_function(function);
@@ -49,7 +52,8 @@ pub(crate) fn optimize_function(function: &mut Function, function_call: &Functio
     if function_call_count > 100 {
         pass_manager.run_passes_with_priority(
             OptimizationPassPriority::Medium,
-            &mut function.code
+            &mut function.code,
+            module
         ).unwrap();
 
         compile_function(function);
@@ -58,7 +62,8 @@ pub(crate) fn optimize_function(function: &mut Function, function_call: &Functio
     if function_call_count > 50 {
         pass_manager.run_passes_with_priority(
             OptimizationPassPriority::Low,
-            &mut function.code
+            &mut function.code,
+            module
         ).unwrap();
 
         compile_function(function);
@@ -71,17 +76,8 @@ pub(crate) fn optimize_function(function: &mut Function, function_call: &Functio
     if function_call_count > 10 {
         pass_manager.run_passes_with_priority(
             OptimizationPassPriority::Min,
-            &mut function.code
+            &mut function.code,
+            module
         ).unwrap();
     }
-}
-
-pub(crate) fn step_function(
-    stack_frame: &frames::StackFrame, 
-    program_counter: usize, 
-    instruction_counter: usize, 
-    function: &mut Function
-) -> bool {
-    let instruction = &function.code[program_counter];
-    true
 }
