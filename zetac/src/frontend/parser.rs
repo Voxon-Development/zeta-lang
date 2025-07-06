74f79758994d1e60b37d0446e830f7865461baba
FlameyosFlow <flamesfflowsnow@gmail.com>
2025-07-06 23:33:49 +0300

    Use new atomic bumps

--- zetac/src/frontend/parser.rs
+++ zetac/src/frontend/parser.rs
@@ -1,23 +1,24 @@
+use ir::bump::AtomicBump;
 use crate::ast;
-use crate::ast::{InternalExprStmt, Stmt};
+use crate::ast::Stmt;
 use crate::frontend::stmt_parser::StmtParser;
-use crate::frontend::tokens::{Token, TokenType};
+use crate::frontend::tokens::Token;
 
 pub struct Parser {
-    stmt_parser: StmtParser<<Vec<Token> as IntoIterator>::IntoIter>,
+    stmt_parser: StmtParser,
     current: usize
 }
 
 impl Parser {
-    pub fn new(tokens: Vec<Token>) -> Parser {
+    pub fn new(tokens: Vec<Token, AtomicBump>) -> Parser {
         Parser {
-            stmt_parser: StmtParser::new(tokens.into_iter()),
+            stmt_parser: StmtParser::new(tokens),
             current: 0
         }
     }
 
-    pub fn parse(mut self) -> Vec<Stmt> {
-        let mut stmts = Vec::new();
+    pub fn parse(mut self) -> Vec<Stmt, AtomicBump> {
+        let mut stmts: Vec<Stmt, AtomicBump> = Vec::with_capacity_in(16, AtomicBump::new());
 
         while !self.stmt_parser.is_at_end() {
             let mut visibility: Option<ast::Visibility> = None;
@@ -49,7 +50,7 @@ impl Parser {
     }
 }
 
-pub fn parse_program(tokens: Vec<Token>) -> Result<Vec<ast::Stmt>, String> {
+pub fn parse_program(tokens: Vec<Token, AtomicBump>) -> Result<Vec<Stmt, AtomicBump>, String> {
     let parser = Parser::new(tokens);
     Ok(parser.parse())
 }
