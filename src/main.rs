816c0aa4db37d4038ac522090a0ac4524d1a2d25
FlameyosFlow <flamesfflowsnow@gmail.com>
2025-07-06 23:30:50 +0300

    New benchmarks for new bump system

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,64 @@
-use std::alloc::{alloc, dealloc, Layout};
-use std::collections::{HashMap, HashSet};
-use std::ptr::NonNull;
+use rand::TryRngCore;
+use ir::bump;
 
-mod benches;
-
-const BLOCKS: usize = 100_000;
-const SIZE: usize = 64;
-
-struct DefaultAllocator;
-
-impl DefaultAllocator {
-    fn alloc(&self, size: usize) -> *mut u8 {
-        let layout = Layout::from_size_align(size, 8).unwrap();
-        unsafe { alloc(layout) }
-    }
-
-    fn free(&self, ptr: *mut u8, size: usize) {
-        let layout = Layout::from_size_align(size, 8).unwrap();
-        unsafe { dealloc(ptr, layout) };
-    }
-}
-
-#[derive(Default)]
-struct MeshAllocator {
-    blocks: HashMap<usize, NonNull<u8>>,
-    graph: HashMap<usize, HashSet<usize>>,
-    next_id: usize,
-}
-
-impl MeshAllocator {
-    fn alloc(&mut self, size: usize, near: Option<usize>) -> usize {
-        let layout = Layout::from_size_align(size, 8).unwrap();
-        let ptr = unsafe { alloc(layout) };
-        let id = self.next_id;
-        self.next_id += 1;
-
-        self.blocks.insert(id, NonNull::new(ptr).unwrap());
-
-        if let Some(n) = near {
-            self.graph.entry(n).or_default().insert(id);
-        }
-
-        id
-    }
-
-    fn access(&self, id: usize) -> *mut u8 {
-        self.blocks.get(&id).unwrap().as_ptr()
-    }
-
-    fn free(&mut self, id: usize, size: usize) {
-        if let Some(ptr) = self.blocks.remove(&id) {
-            let layout = Layout::from_size_align(size, 8).unwrap();
-            unsafe { dealloc(ptr.as_ptr(), layout) };
-        }
-        self.graph.remove(&id);
-    }
+#[derive(Debug)]
+struct A {
+    a: i32,
+    b: i32,
+    c: i32,
 }
 
 fn main() {
+    // random int
+    let random_int = rand::rngs::OsRng.try_next_u64().unwrap();
     let start = std::time::Instant::now();
-    let allocator = DefaultAllocator;
-    let mut ptrs = Vec::with_capacity(BLOCKS);
-    for _ in 0..BLOCKS {
-        let ptr = allocator.alloc(SIZE);
-        unsafe { std::ptr::write_bytes(ptr, 0xaa, SIZE) }; // simulate access
-        ptrs.push(ptr);
+    let bumpalo = bumpalo::Bump::new();
+
+    for i in 0..10000 {
+        let s = bumpalo.alloc(A {
+            a: i as i32,
+            b: i as i32,
+            c: i as i32,
+        });
+        println!("{:?}", s);
     }
-    for ptr in ptrs {
-        allocator.free(ptr, SIZE);
-    }
-    println!("{:?}", start.elapsed().as_nanos());
-    
+    drop(bumpalo);
+    let end1 = start.elapsed().as_millis();
+
     let start = std::time::Instant::now();
-    let mut mesh = MeshAllocator::default();
-    let mut ids = Vec::with_capacity(BLOCKS);
-    for i in 0..BLOCKS {
-        let near = if i > 0 { Some(i - 1) } else { None };
-        let id = mesh.alloc(SIZE, near);
+    for i in 0..10000 {
+        let s = Box::new(A {
+            a: i as i32,
+            b: i as i32,
+            c: i as i32,
+        });
+        println!("{:?}", s);
+    }
+    let end2 = start.elapsed().as_millis();
+
+    let start = std::time::Instant::now();
+    let bump = bump::Bump::new();
+    let max: i32 = 10000;
+    for i in 0..max {
+        let s = bump.alloc(size_of::<A>(), align_of::<A>());
         unsafe {
-            std::ptr::write_bytes(mesh.access(id), 0xaa, SIZE); // simulate access
+            *(s as *mut A) = A {
+                a: i as i32,
+                b: i as i32,
+                c: i as i32,
+            };
         }
-        ids.push(id);
+
+        println!("{:?}", unsafe { &*s });
     }
-    for id in ids {
-        mesh.free(id, SIZE);
-    }
-    println!("{:?}", start.elapsed().as_nanos());
+    drop(bump);
+    let end3 = start.elapsed().as_millis();
+
+    println!("bumpalo: {}ms", end1);
+    println!("box: {}ms", end2);
+    println!("custom: {}ms", end3);
+}
+
+pub fn use_i32(i: i32) -> i32 {
+    i
 }
