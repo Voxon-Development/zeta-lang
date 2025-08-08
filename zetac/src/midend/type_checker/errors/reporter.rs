2b51e746566c6b1645f3f435157a4f34c560f23e
FlameyosFlow <flamesfflowsnow@gmail.com>
2025-08-08 11:57:42 +0300

    Write a working error reporter

--- zetac/src/midend/type_checker/errors/reporter.rs
+++ zetac/src/midend/type_checker/errors/reporter.rs
@@ -1,10 +1,30 @@
 use crate::midend::type_checker::errors::error::TypeError;
 
+pub struct ErrorReporter {
+    pub errors: Vec<TypeError>,
+}
+
 // Centralized error reporting for type-checking.
-pub trait ErrorReporter {
+impl ErrorReporter {
+    pub const fn new() -> ErrorReporter {
+        ErrorReporter {
+            errors: Vec::new(),
+        }
+    }
+    
     // Reports a type error with location info and diagnostic hints.
-    fn report(&mut self, error: TypeError);
+    pub fn add_error(&mut self, error: TypeError) {
+        self.errors.push(error);
+    }
+    
+    pub fn report_current_reports(&mut self) {
+        for error in &self.errors {
+            eprintln!("{}", error);
+        }
+    }
 
     // Checks if any errors were reported (for early bailout).
-    fn has_errors(&self) -> bool;
+    pub fn has_errors(&self) -> bool {
+        self.errors.len() > 0
+    }
 }
