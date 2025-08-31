dec35aee4e8b9afb955b5e155acf0c582c8ea901
FlameyosFlow <flamesfflowsnow@gmail.com>
2025-08-31 22:37:52 +0300

    New type checker API

---
+++ sentinel-typechecker/src/rules/functions/no_void_return.rs
@@ -0,0 +1,24 @@
+use std::ops::Deref;
+use ir::ast::{FuncDecl, Stmt, Type};
+use ir::errors::error::{SourceSpan, TypeError};
+use crate::rules::rule::TypeRule;
+use crate::type_checker::TypeChecker;
+
+pub struct NoVoidReturnRule;
+
+impl TypeRule for NoVoidReturnRule {
+    fn check_func(&self, ctx: &mut TypeChecker, func: &FuncDecl) {
+        let Some(Type::Void) = func.return_type else { return; };
+        let Some(ref body) = func.body else { return; };
+
+        for stmt in &body.block {
+            let Stmt::Return(return_stmt) = stmt else { continue };
+            let Some(ref return_value) = return_stmt.value else { continue };
+            ctx.ctx.error_reporter.add_error(TypeError::VoidFunctionWithNonVoidReturn {
+                function_name: func.name.clone(),
+                return_value: return_value.deref().clone(),
+                location: SourceSpan::default()
+            })
+        }
+    }
+}
