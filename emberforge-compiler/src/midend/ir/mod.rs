b35bd3fe4f6ec7bde560230ef9d97874ed08e336
FlameyosFlow <89988527+FlameyosSnowy@users.noreply.github.com>
2025-09-19 12:46:03 +0300

    Made optimized_string_buffering and ir_conversion public

--- emberforge-compiler/src/midend/ir/mod.rs
+++ emberforge-compiler/src/midend/ir/mod.rs
@@ -3,5 +3,5 @@ pub mod module_lowerer;
 pub mod block_data;
 pub mod expr_lowerer;
 
-mod optimized_string_buffering;
-mod ir_conversion;
+pub mod optimized_string_buffering;
+pub mod ir_conversion;
