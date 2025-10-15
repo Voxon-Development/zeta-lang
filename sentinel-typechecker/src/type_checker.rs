bdd6b9f9e435cd481b69430794283bb3449b5513
flameyosflow <flamesfflowsnow@gmail.com>
2025-10-15 16:40:38 +0300

    Use bumps, replace cryptographic hashers with faster hashers and adjust to new AST

--- sentinel-typechecker/src/type_checker.rs
+++ sentinel-typechecker/src/type_checker.rs
@@ -1,33 +1,38 @@
-use std::cell::RefCell;
 use crate::rules::rule::{TypeCheckerRule, TypeRule};
 use crate::type_checker_ctx::TypeCheckerCtx;
-use ir::ast::{Block, ElseBranch, Expr, ForStmt, FuncDecl, IfStmt, LetStmt, Op, ReturnStmt, Stmt, Type, WhileStmt};
-use std::collections::HashMap;
-use std::rc::Rc;
 use ir::ast::Type::I8;
+use ir::ast::{
+    Block, ElseBranch, Expr, ForStmt, FuncDecl, IfStmt, LetStmt, Op, ReturnStmt, Stmt, Type,
+    WhileStmt,
+};
 use ir::context::Context;
 use ir::hir::StrId;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::Rc;
+use ir::ir_hasher::FxHashBuilder;
+use zetaruntime::bump::GrowableBump;
 
 pub struct TypeChecker<'a> {
-    rules: Vec<TypeCheckerRule>,
+    rules: Vec<TypeCheckerRule, GrowableBump>,
     pub ctx: TypeCheckerCtx,
-    symbol_table: HashMap<StrId, Type>,
-    pub context: Rc<RefCell<Context<'a>>>
+    symbol_table: HashMap<StrId, Type, FxHashBuilder>,
+    pub context: Rc<RefCell<Context<'a>>>,
 }
 
 impl<'a> TypeChecker<'a> {
     pub fn new(context: Rc<RefCell<Context<'a>>>) -> Self {
         Self {
-            rules: Vec::new(),
+            rules: Vec::new_in(GrowableBump::new(size_of::<TypeCheckerRule>() * 1024, align_of::<TypeCheckerRule>())),
             ctx: TypeCheckerCtx::new(),
-            symbol_table: HashMap::new(),
-            context
+            symbol_table: HashMap::with_hasher(FxHashBuilder),
+            context,
         }
     }
 
     pub fn get_expr_type(&mut self, expr: &Expr) -> Type {
         match expr {
-            _ => I8
+            _ => I8,
         }
     }
 
@@ -37,46 +42,61 @@ impl<'a> TypeChecker<'a> {
 
     pub fn check_program(&mut self, stmts: &[Stmt]) -> Result<(), Vec<String>> {
         let mut errors = Vec::new();
-        
+
         // First pass: collect declarations
         for stmt in stmts {
             if let Err(e) = self.collect_declarations(stmt) {
                 errors.push(e);
             }
         }
-        
+
         // Second pass: type check
         for stmt in stmts {
             if let Err(e) = self.check_stmt(stmt) {
                 errors.push(e);
             }
         }
-        
+
         if errors.is_empty() {
             Ok(())
         } else {
             Err(errors)
         }
     }
-    
+
     fn collect_declarations(&mut self, stmt: &Stmt) -> Result<(), String> {
         match stmt {
-            Stmt::Let(LetStmt { ident, type_annotation, .. }) => {
-                self.symbol_table.insert(ident.clone(), type_annotation.clone());
+            Stmt::Let(LetStmt {
+                ident,
+                type_annotation,
+                ..
+            }) => {
+                self.symbol_table
+                    .insert(ident.clone(), type_annotation.clone());
                 Ok(())
             }
-            Stmt::FuncDecl(FuncDecl { name, params, return_type, .. }) => {
+            Stmt::FuncDecl(FuncDecl {
+                name,
+                params: _,
+                return_type: _,
+                ..
+            }) => {
                 // TODO: Handle function types properly
                 self.symbol_table.insert(name.clone(), Type::Void);
                 Ok(())
             }
-            _ => Ok(())
+            _ => Ok(()),
         }
     }
-    
+
     fn check_stmt(&mut self, stmt: &Stmt) -> Result<(), String> {
         match stmt {
-            Stmt::Let(LetStmt { ident, value, type_annotation, .. }) => {
+            Stmt::Let(LetStmt {
+                ident: _,
+                value,
+                type_annotation,
+                ..
+            }) => {
                 let value_ty = self.check_expr(value)?;
                 if &value_ty != type_annotation {
                     return Err(format!(
@@ -92,13 +112,19 @@ impl<'a> TypeChecker<'a> {
                 }
                 Ok(())
             }
-            Stmt::If(IfStmt { condition, then_branch, else_branch }) => {
+            Stmt::If(IfStmt {
+                condition,
+                then_branch,
+                else_branch,
+            }) => {
                 self.check_condition(condition)?;
                 self.check_block(then_branch)?;
                 if let Some(else_branch) = else_branch {
                     match else_branch.as_ref() {
                         ElseBranch::Else(block) => self.check_block(block)?,
-                        ElseBranch::If(if_stmt) => self.check_stmt(&Stmt::If(if_stmt.as_ref().clone()))?,
+                        ElseBranch::If(if_stmt) => {
+                            self.check_stmt(&Stmt::If(if_stmt.as_ref().clone()))?
+                        }
                     }
                 }
                 Ok(())
@@ -107,7 +133,12 @@ impl<'a> TypeChecker<'a> {
                 self.check_condition(condition)?;
                 self.check_block(block)
             }
-            Stmt::For(ForStmt { let_stmt, condition, increment, block }) => {
+            Stmt::For(ForStmt {
+                let_stmt,
+                condition,
+                increment,
+                block,
+            }) => {
                 if let Some(let_stmt) = let_stmt {
                     self.check_stmt(&Stmt::Let(let_stmt.clone()))?;
                 }
@@ -126,26 +157,26 @@ impl<'a> TypeChecker<'a> {
             _ => Ok(()), // Handle other statements
         }
     }
-    
+
     fn check_block(&mut self, block: &Block) -> Result<(), String> {
         let mut errors = Vec::new();
         let old_scope = self.symbol_table.clone();
-        
+
         for stmt in &block.block {
             if let Err(e) = self.check_stmt(stmt) {
                 errors.push(e);
             }
         }
-        
+
         self.symbol_table = old_scope;
-        
+
         if errors.is_empty() {
             Ok(())
         } else {
             Err(errors.join("\n"))
         }
     }
-    
+
     pub fn check_condition(&mut self, expr: &Expr) -> Result<(), String> {
         let ty = self.check_expr(expr)?;
         if ty != Type::Boolean {
@@ -153,18 +184,18 @@ impl<'a> TypeChecker<'a> {
         }
         Ok(())
     }
-    
+
     pub fn check_expr(&mut self, expr: &Expr) -> Result<Type, String> {
         match expr {
-            Expr::Number(_) => Ok(Type::I32),
-            Expr::Decimal(_) => Ok(Type::F64),
-            Expr::Boolean(_) => Ok(Type::Boolean),
-            Expr::String(_) => Ok(Type::String),
-            Expr::Char(_) => Ok(Type::Char),
-            Expr::Binary { left, op, right } => {
+            Expr::Number { .. } => Ok(Type::I32),
+            Expr::Decimal { .. } => Ok(Type::F64),
+            Expr::Boolean { .. } => Ok(Type::Boolean),
+            Expr::String { .. } => Ok(Type::String),
+            Expr::Char { .. } => Ok(Type::Char),
+            Expr::Binary { left, op, right, .. } => {
                 let left_ty = self.check_expr(left)?;
                 let right_ty = self.check_expr(right)?;
-                
+
                 match op {
                     Op::Add | Op::Sub | Op::Mul | Op::Div | Op::Mod => {
                         if left_ty.is_numeric() && right_ty.is_numeric() {
@@ -179,33 +210,39 @@ impl<'a> TypeChecker<'a> {
                                 Ok(Type::I32) // Default to i32 for integer literals
                             }
                         } else {
-                            Err(format!("Cannot apply {:?} to {} and {}", op, left_ty, right_ty))
+                            Err(format!(
+                                "Cannot apply {:?} to {} and {}",
+                                op, left_ty, right_ty
+                            ))
                         }
-                    },
+                    }
                     Op::Eq | Op::Neq | Op::Lt | Op::Lte | Op::Gt | Op::Gte => {
                         if left_ty == right_ty {
                             Ok(Type::Boolean)
                         } else {
                             Err(format!("Cannot compare {} with {}", left_ty, right_ty))
                         }
-                    },
+                    }
                     _ => Err(format!("Unsupported operation: {:?}", op)),
                 }
-            },
-            Expr::Ident(name) => {
-                self.symbol_table.get(name)
-                    .cloned()
-                    .ok_or_else(|| format!("Undefined variable: {}", name))
-            },
-            Expr::Call { callee, arguments } => {
+            }
+            Expr::Ident { name, .. } => self
+                .symbol_table
+                .get(name)
+                .cloned()
+                .ok_or_else(|| format!("Undefined variable: {}", self.context.borrow_mut().string_pool.resolve_string(&*name))),
+            Expr::Call {
+                callee: _,
+                arguments, ..
+            } => {
                 // For now, just check the arguments
                 for arg in arguments {
                     self.check_expr(arg)?;
                 }
 
                 Ok(Type::Void)
-            },
-            _ => Ok(Type::Void)
+            }
+            _ => Ok(Type::Void),
         }
     }
 }
--- sentinel-typechecker/src/type_checker_ctx.rs
+++ sentinel-typechecker/src/type_checker_ctx.rs
@@ -3,16 +3,22 @@ use std::collections::HashMap;
 use ir::errors::reporter::ErrorReporter;
 use ir::{ast::{ClassDecl, InterfaceDecl}, ssa_ir::Value};
 use ir::hir::StrId;
+use ir::ir_hasher::FxHashBuilder;
 
 pub struct TypeCheckerCtx {
-    var_map: HashMap<StrId, Value>,
-    classes: HashMap<StrId, ClassDecl>,
-    interfaces: HashMap<StrId, InterfaceDecl>,
+    var_map: HashMap<StrId, Value, FxHashBuilder>,
+    classes: HashMap<StrId, ClassDecl, FxHashBuilder>,
+    interfaces: HashMap<StrId, InterfaceDecl, FxHashBuilder>,
     pub error_reporter: ErrorReporter
 }
 
 impl TypeCheckerCtx {
     pub fn new() -> Self {
-        TypeCheckerCtx { var_map: HashMap::new(), classes: HashMap::new(), interfaces: HashMap::new(), error_reporter: ErrorReporter::new() }
+        TypeCheckerCtx {
+            var_map: HashMap::with_hasher(FxHashBuilder),
+            classes: HashMap::with_hasher(FxHashBuilder),
+            interfaces: HashMap::with_hasher(FxHashBuilder),
+            error_reporter: ErrorReporter::new()
+        }
     }
 }
