42f2a729bc769e800d3790f2f7e4c0cf8459fcf0
flameyosflow <flamesfflowsnow@gmail.com>
2025-10-15 17:01:18 +0300

    Add funcs, make the code cleaner, and add lifetimes, adjusting for new syntax

--- emberforge-compiler/src/midend/ir/lowerer.rs
+++ emberforge-compiler/src/midend/ir/lowerer.rs
@@ -1,49 +1,61 @@
 use crate::midend::ir::block_data::CurrentBlockData;
 use crate::midend::ir::expr_lowerer::MirExprLowerer;
-use ir::hir::{Hir, HirClass, HirExpr, HirFunc, HirInterface, HirModule, HirStmt, HirType, Operator, StrId};
-use ir::ir_hasher::FxHashBuilder;
-use ir::ssa_ir::{
-    BasicBlock, BinOp, BlockId, Function, Instruction, Operand, SsaType, Value,
-};
-use smallvec::SmallVec;
-use std::collections::HashMap;
-use zetaruntime::string_pool::StringPool;
 use crate::midend::ir::ir_conversion::lower_type_hir;
+use ir::context::Context;
+use ir::hir::{HirClass, HirExpr, HirFunc, HirParam, HirStmt, HirType, StrId};
+use ir::ir_hasher::FxHashBuilder;
+use ir::ssa_ir::{BasicBlock, BlockId, Function, Instruction, Operand, SsaType, Value};
+use smallvec::SmallVec;
+use std::cell::RefCell;
+use std::collections::HashMap;
+use std::rc::Rc;
 
-pub struct FunctionLowerer {
+pub struct FunctionLowerer<'a> {
     current_block_data: CurrentBlockData,
     var_map: HashMap<StrId, Value, FxHashBuilder>,
 
     // immutable metadata snapshots
-    class_field_offsets: HashMap<StrId, HashMap<StrId, usize, FxHashBuilder>, FxHashBuilder>,
-    class_method_slots: HashMap<StrId, HashMap<StrId, usize, FxHashBuilder>, FxHashBuilder>,
-    class_mangled_map: HashMap<StrId, HashMap<StrId, StrId, FxHashBuilder>, FxHashBuilder>,
-    class_vtable_slots: HashMap<StrId, Vec<StrId>, FxHashBuilder>,
-    interface_id_map: HashMap<StrId, usize, FxHashBuilder>,
-    interface_method_slots: HashMap<StrId, HashMap<StrId, usize, FxHashBuilder>, FxHashBuilder>,
-    classes: HashMap<StrId, HirClass, FxHashBuilder>,
+    funcs: &'a HashMap<StrId, Function, FxHashBuilder>,
+    class_field_offsets: &'a HashMap<StrId, HashMap<StrId, usize, FxHashBuilder>, FxHashBuilder>,
+    class_method_slots: &'a HashMap<StrId, HashMap<StrId, usize, FxHashBuilder>, FxHashBuilder>,
+    class_mangled_map: &'a HashMap<StrId, HashMap<StrId, StrId, FxHashBuilder>, FxHashBuilder>,
+    class_vtable_slots: &'a HashMap<StrId, Vec<StrId>, FxHashBuilder>,
+    interface_id_map: &'a HashMap<StrId, usize, FxHashBuilder>,
+    interface_method_slots: &'a HashMap<StrId, HashMap<StrId, usize, FxHashBuilder>, FxHashBuilder>,
+    classes: &'a HashMap<StrId, HirClass, FxHashBuilder>,
 
-    string_pool: StringPool
+    /// Context is `'static` here by design (see notes below).
+    context: Rc<RefCell<Context<'static>>>,
 }
 
-impl FunctionLowerer {
+impl<'a> FunctionLowerer<'a> {
     pub fn new(
         hir_fn: &HirFunc,
-        class_field_offsets: &HashMap<StrId, HashMap<StrId, usize, FxHashBuilder>, FxHashBuilder>,
-        class_method_slots: &HashMap<StrId, HashMap<StrId, usize, FxHashBuilder>, FxHashBuilder>,
-        class_mangled_map: &HashMap<StrId, HashMap<StrId, StrId, FxHashBuilder>, FxHashBuilder>,
-        class_vtable_slots: &HashMap<StrId, Vec<StrId>, FxHashBuilder>,
-        interface_id_map: &HashMap<StrId, usize, FxHashBuilder>,
-        interface_method_slots: &HashMap<StrId, HashMap<StrId, usize, FxHashBuilder>, FxHashBuilder>,
-        classes: &HashMap<StrId, HirClass, FxHashBuilder>,
+        funcs: &'a HashMap<StrId, Function, FxHashBuilder>,
+        class_field_offsets: &'a HashMap<
+            StrId,
+            HashMap<StrId, usize, FxHashBuilder>,
+            FxHashBuilder,
+        >,
+        class_method_slots: &'a HashMap<StrId, HashMap<StrId, usize, FxHashBuilder>, FxHashBuilder>,
+        class_mangled_map: &'a HashMap<StrId, HashMap<StrId, StrId, FxHashBuilder>, FxHashBuilder>,
+        class_vtable_slots: &'a HashMap<StrId, Vec<StrId>, FxHashBuilder>,
+        interface_id_map: &'a HashMap<StrId, usize, FxHashBuilder>,
+        interface_method_slots: &'a HashMap<
+            StrId,
+            HashMap<StrId, usize, FxHashBuilder>,
+            FxHashBuilder,
+        >,
+        classes: &'a HashMap<StrId, HirClass, FxHashBuilder>,
+        context: Rc<RefCell<Context<'static>>>,
     ) -> Result<Self, std::alloc::AllocError> {
-        let mut func = Function { 
-            name: hir_fn.name.clone(), 
+        let mut func = Function {
+            name: hir_fn.name.clone(),
             params: SmallVec::new(),
-            ret_type: lower_type_hir(hir_fn.return_type.as_ref().unwrap_or(&HirType::Void)), 
+            ret_type: lower_type_hir(hir_fn.return_type.as_ref().unwrap_or(&HirType::Void)),
             blocks: SmallVec::new(),
-            value_types: HashMap::with_hasher(FxHashBuilder), 
-            entry: BlockId(0) 
+            value_types: HashMap::with_hasher(FxHashBuilder),
+            entry: BlockId(0),
         };
 
         let mut next_value = 0usize;
@@ -52,33 +64,63 @@ impl FunctionLowerer {
         let mut value_types = HashMap::with_hasher(FxHashBuilder);
         // allocate params
         for p in &hir_fn.params {
-            let v = Value(next_value); next_value += 1;
-            let ty = lower_type_hir(&p.param_type);
+            let v = Value(next_value);
+            next_value += 1;
+
+            let (name, param_type) = match p {
+                HirParam::Normal { name, param_type } => (*name, param_type),
+                HirParam::This { param_type } => {
+                    let this_str_id = StrId(context.borrow_mut().string_pool.intern("this"));
+                    (this_str_id, param_type.as_ref().unwrap_or(&HirType::This))
+                }
+            };
+            let ty: SsaType = lower_type_hir(param_type);
             func.params.push((v, ty.clone()));
             value_types.insert(v, ty);
-            var_map.insert(p.name.clone(), v);
+            var_map.insert(name, v);
         }
+
         // create entry block
-        let entry_bb = BlockId(next_block); 
+        let entry_bb = BlockId(next_block);
         next_block += 1;
         func.entry = entry_bb;
-        func.blocks.push(BasicBlock { id: entry_bb, instructions: Vec::new() });
+        func.blocks.push(BasicBlock {
+            id: entry_bb,
+            instructions: Vec::new(),
+        });
+
         Ok(Self {
-            current_block_data: CurrentBlockData::new(func, entry_bb, next_value, next_block, value_types),
+            current_block_data: CurrentBlockData::new(
+                func,
+                entry_bb,
+                next_value,
+                next_block,
+                value_types,
+            ),
+            funcs,
             var_map,
-            class_field_offsets: class_field_offsets.clone(),
-            class_method_slots: class_method_slots.clone(),
-            class_mangled_map: class_mangled_map.clone(),
-            class_vtable_slots: class_vtable_slots.clone(),
-            interface_id_map: interface_id_map.clone(),
-            interface_method_slots: interface_method_slots.clone(),
-            classes: classes.clone(),
-            string_pool: StringPool::new()?
+            class_field_offsets,
+            class_method_slots,
+            class_mangled_map,
+            class_vtable_slots,
+            interface_id_map,
+            interface_method_slots,
+            classes,
+            context,
         })
     }
 
     pub(super) fn lower_body(&mut self, body: Option<&HirStmt>) {
-        if let Some(b) = body { match b { HirStmt::Block { body } => { for stmt in body { self.lower_stmt(stmt); } }, _ => unimplemented!("Function body must be a block"), } }
+        if let Some(b) = body {
+            match b {
+                HirStmt::Block { body } => {
+                    for stmt in body {
+                        self.lower_stmt(stmt);
+                    }
+                }
+                _ => unimplemented!("Function body must be a block"),
+            }
+        }
     }
 
     pub(super) fn lower_stmt(&mut self, stmt: &HirStmt) {
@@ -92,7 +134,10 @@ impl FunctionLowerer {
                     let val = self.allow_lowering_expr(e);
                     Operand::Value(val)
                 });
-                self.current_block_data.bb().instructions.push(Instruction::Ret { value });
+                self.current_block_data
+                    .bb()
+                    .instructions
+                    .push(Instruction::Ret { value });
             }
             HirStmt::Expr(expr) => {
                 let _ = self.allow_lowering_expr(expr);
@@ -102,21 +147,20 @@ impl FunctionLowerer {
     }
 
     fn allow_lowering_expr(&mut self, value: &HirExpr) -> Value {
-        let val = {
-            let mut el = MirExprLowerer::new(
-                &mut self.current_block_data,
-                &mut self.var_map,
-                &mut self.string_pool,
-                &self.class_field_offsets,
-                &self.class_method_slots,
-                &self.class_mangled_map,
-                &self.class_vtable_slots,
-                &self.interface_id_map,
-                &self.interface_method_slots,
-                &self.classes
-            );
-            el.lower_expr(value)
-        };
+        let mut el = MirExprLowerer::new(
+            &mut self.current_block_data,
+            &self.funcs,
+            &mut self.var_map,
+            self.context.clone(),
+            &self.class_field_offsets,
+            &self.class_method_slots,
+            &self.class_mangled_map,
+            &self.class_vtable_slots,
+            &self.interface_id_map,
+            &self.interface_method_slots,
+            &self.classes,
+        );
+        let val = el.lower_expr(value);
         val
     }
 
