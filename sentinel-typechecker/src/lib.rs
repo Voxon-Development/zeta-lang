b616667d0cabf3be3ab6821b41458b3437d73adc
flameyosflow <flamesfflowsnow@gmail.com>
2025-12-14 15:13:38 +0200

    Basic type checking support.

--- sentinel-typechecker/src/lib.rs
+++ sentinel-typechecker/src/lib.rs
@@ -1,1 +1,9 @@
 #![feature(allocator_api)]
+
+pub mod type_checker;
+pub mod type_context;
+pub mod type_error;
+
+pub use type_checker::TypeChecker;
+pub use type_context::TypeContext;
+pub use type_error::{TypeError, TypeCheckResult};
