83f8f3ea056aea6ed735ffa09c05a5056ab3db8c
FlameyosSnowy <flamesfflowsnow@gmail.com>
2026-07-13 19:25:55 +0300

    Listen to should_exit and specify all important LSP capabilities

--- zeta-lsp/src/server.rs
+++ zeta-lsp/src/server.rs
@@ -1,5 +1,11 @@
 use anyhow::Result;
 use lsp_server::{Connection, Message};
+use lsp_types::{
+    CompletionOptions, HoverProviderCapability, SaveOptions, SemanticTokensFullOptions,
+    SemanticTokensOptions, SemanticTokensServerCapabilities, ServerCapabilities,
+    TextDocumentSyncCapability, TextDocumentSyncKind, TextDocumentSyncOptions,
+    TextDocumentSyncSaveOptions,
+};
 
 use crate::{dispatch, state::ServerState};
 
@@ -21,17 +27,48 @@ where
                 compiler,
                 workspace_root: None,
                 documents: Default::default(),
+                should_exit: false,
             },
         })
     }
 
     pub fn run(mut self) -> Result<()> {
-        let capabilities = serde_json::json!({
-            // I'll fill these in later
-        });
+        let capabilities = serde_json::to_value(ServerCapabilities {
+            text_document_sync: Some(TextDocumentSyncCapability::Options(
+                TextDocumentSyncOptions {
+                    open_close: Some(true),
+                    change: Some(TextDocumentSyncKind::FULL),
+                    save: Some(TextDocumentSyncSaveOptions::SaveOptions(SaveOptions {
+                        include_text: Some(false),
+                    })),
+                    ..Default::default()
+                },
+            )),
+            document_symbol_provider: Some(lsp_types::OneOf::Left(true)),
+            definition_provider: Some(lsp_types::OneOf::Left(true)),
+            hover_provider: Some(HoverProviderCapability::Simple(true)),
+            completion_provider: Some(CompletionOptions {
+                trigger_characters: Some(vec![".".to_string(), "::".to_string()]),
+                resolve_provider: Some(false), // set true later if we add completionItem/resolve
+                ..Default::default()
+            }),
+            semantic_tokens_provider: Some(
+                SemanticTokensServerCapabilities::SemanticTokensOptions(SemanticTokensOptions {
+                    legend: crate::semantic_tokens::legend(),
+                    full: Some(SemanticTokensFullOptions::Bool(true)),
+                    range: None,
+                    work_done_progress_options: Default::default(),
+                }),
+            ),
+            rename_provider: Some(lsp_types::OneOf::Right(lsp_types::RenameOptions {
+                prepare_provider: Some(false),
+                work_done_progress_options: Default::default(),
+            })),
+            ..Default::default()
+        })
+        .unwrap();
 
         let initialize_params = self.connection.initialize(capabilities)?;
-
         dispatch::initialize(&mut self.state, initialize_params)?;
 
         for message in &self.connection.receiver {
@@ -40,20 +77,19 @@ where
                     if self.connection.handle_shutdown(&req)? {
                         break;
                     }
-
                     dispatch::request(&self.connection, &mut self.state, &req)?;
                 }
-
                 Message::Notification(notification) => {
                     dispatch::notification(&self.connection, &mut self.state, &notification)?;
+                    if self.state.should_exit {
+                        break;
+                    }
                 }
-
                 Message::Response(response) => {
                     dispatch::response(&mut self.state, &response)?;
                 }
             }
         }
-
         Ok(())
     }
 }
