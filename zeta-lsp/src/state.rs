ffb3375876f2b44fa7aa8bb98a2eba7a491cd473
FlameyosSnowy <flamesfflowsnow@gmail.com>
2026-07-13 19:25:22 +0300

    Add should_exit field

--- zeta-lsp/src/state.rs
+++ zeta-lsp/src/state.rs
@@ -1,6 +1,5 @@
-use std::{collections::HashMap, path::PathBuf};
-
 use lsp_types::Uri;
+use std::{collections::HashMap, path::PathBuf};
 use zeta_compiler_api::Compiler;
 
 pub struct Document {
@@ -10,8 +9,7 @@ pub struct Document {
 
 pub struct ServerState<'a, 'bump> {
     pub compiler: Compiler<'a, 'bump>,
-
     pub workspace_root: Option<PathBuf>,
-
     pub documents: HashMap<Uri, Document>,
+    pub should_exit: bool,
 }
